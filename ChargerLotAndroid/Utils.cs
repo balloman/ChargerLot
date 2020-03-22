c39d1b1f113f16b386a5ea04bccfd9dc3d4ecd8e
Bernard Allotey <benard.allotey4@gmail.com>
2020-03-22 11:12:27 -0500

    MVP Push

---
+++ ChargerLotAndroid/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+
+namespace ChargerLotAndroid
+{
+    public class Utils
+    {
+        public static LatLng ConvertLatLng(Google.Type.LatLng l) => new LatLng(l.Latitude, l.Longitude);
+    }
+}
---
+++ ChargerLotLib/Models/Lot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Type;
+
+namespace ChargerLotLib.Models
+{
+    public struct Lot
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Spaces { get; set; }
+        public LatLng LatLng { get; set; }
+    }
+}
